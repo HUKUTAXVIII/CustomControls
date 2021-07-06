b861d30d8935845f881e97d48bcc8b8e8906da52
Nikita <nikita.dmitrovich@gmail.com>
2021-07-06 11:39:10 +0300

    Add Functional

--- CompassControl.cs
+++ CompassControl.cs
@@ -11,6 +11,7 @@ namespace CustomControls
     public class CompassControl:Control
     {
         public List<string> Navigation;
+        public Point mousePos;
         public CompassControl():base()
         {
             this.Size = new System.Drawing.Size(100,100);
@@ -22,8 +23,16 @@ namespace CustomControls
                 "III",
                 "IV"
             };
+            this.MouseClick += CompassControl_MouseClick;
             
         }
+
+        private void CompassControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.mousePos = new Point(e.X, e.Y);
+            this.Invalidate();
+        }
+
 
         private void CompassControl_Paint(object sender, PaintEventArgs e)
         {
@@ -36,14 +45,40 @@ namespace CustomControls
             Graphics.DrawString(this.Navigation[3],font,Brushes.Blue,0,this.Height/2-12);
 
 
-            //Graphics.FillClosedCurve(Brushes.Blue,new Point[] { 
-            //    new Point(0,0),
-            //    new Point(0,Width),
-            //    new Point(Width/2,Height/2)
-            //});
-
-
             Graphics.FillEllipse(Brushes.White,new Rectangle(this.Width/6,this.Height/6,this.Width- this.Width / 3,this.Height- this.Height/3));
+            if (new Rectangle(this.mousePos, new Size(1, 1)).IntersectsWith(new Rectangle(0, 12, 12, this.Width - 24))) {
+                Graphics.FillClosedCurve(Brushes.Blue,new Point[]{ 
+                    new Point(this.Width/2,this.Height/2-6),
+                    new Point(this.Width/2,this.Height/2+6),
+                    new Point(12,this.Height/2)
+                });
+            }
+            if (new Rectangle(this.mousePos, new Size(1, 1)).IntersectsWith(new Rectangle(12, 0, this.Width-24, 12)))
+            {
+                Graphics.FillClosedCurve(Brushes.Blue, new Point[]{
+                    new Point(this.Width/2-6,this.Height/2),
+                    new Point(this.Width/2+6,this.Height/2),
+                    new Point(this.Width/2,12)
+                });
+            }
+            if (new Rectangle(this.mousePos, new Size(1, 1)).IntersectsWith(new Rectangle(12, this.Height-24, this.Height-12,12)))
+            {
+                Graphics.FillClosedCurve(Brushes.Blue, new Point[]{
+                    new Point(this.Width/2-6,this.Height/2),
+                    new Point(this.Width/2+6,this.Height/2),
+                    new Point(this.Width/2,this.Height-12)
+                });
+            }
+            if (new Rectangle(this.mousePos, new Size(1, 1)).IntersectsWith(new Rectangle(this.Width-12, 12, 12, this.Height-24)))
+            {
+                Graphics.FillClosedCurve(Brushes.Blue, new Point[]{
+                    new Point(this.Width/2,this.Height/2-6),
+                    new Point(this.Width/2,this.Height/2+6),
+                    new Point(this.Width-12,this.Height/2)
+                });
+            }
+
+
 
         }
     }
