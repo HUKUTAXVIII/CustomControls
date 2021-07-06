392a7f8e1a5d369f0333d57d4a8d8498c8342e5e
Nikita <nikita.dmitrovich@gmail.com>
2021-07-06 09:20:56 +0300

    Init Commit

--- Program.cs
+++ Program.cs
@@ -17,6 +17,10 @@ namespace CustomControls
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
+
         }
     }
 }
